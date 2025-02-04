4bc130ba1dbe2e8ee98b14a75047f45a3512bf97
Sushanta9777 <smeher9777@gmail.com>
2025-02-04 21:25:49 +0530

    E-commerce

---
+++ e-commerce-admin/src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <hr />
+      <p>Copyright @ 2024 - All Right Reserved.</p>
+      <p>Devloped By Mr. Sushanta meher</p>
+    </div>
+  )
+}
+
+export default Footer
